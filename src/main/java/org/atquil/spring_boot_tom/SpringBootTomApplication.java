f2fc4a861451d5bb6f2beaa87fc27cc08a0f2c2c
ATQUIL <creator.atquil@gmail.com>
2024-08-10 15:46:12 +0530

    first commit

---
+++ src/main/java/org/atquil/spring_boot_tom/SpringBootTomApplication.java
@@ -0,0 +1,13 @@
+package org.atquil.spring_boot_tom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/atquil/spring_boot_tom/SpringBootTomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atquil.spring_boot_tom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
